ee14b5532db44d61f8ce7867cf9e4e0a0e4912b0
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-05-24 09:56:42 -0700

    species  table created

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -10,10 +10,30 @@ CREATE DATABASE  conservation_db ;
     region VARCHAR(100)
  );
 
+-- Create species table
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(150),
+    discovery_date DATE,
+    conservation_status VARCHAR(50)
+);
+
 
  INSERT INTO rangers (name, region) VALUES 
 ('Alice Green', 'Northern Hills'),
 ('Bob White', 'River Delta'),
 ('Carol King', 'Mountain Range');
 
-SELECT * FROM rangers 
+
+-- Insert species
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES
+('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+('Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+
+SELECT * FROM rangers ;
+
+
+SELECT * FROM species ;
