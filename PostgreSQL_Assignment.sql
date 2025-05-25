e757f38f942b0ce2c9db6c11dc1b30b589b157aa
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-05-24 21:11:05 -0700

    create all table

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -19,6 +19,18 @@ CREATE TABLE species (
     conservation_status VARCHAR(50)
 );
 
+   -- Create  sightings table
+   CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    species_id INTEGER REFERENCES species(species_id),
+    ranger_id  INTEGER REFERENCES rangers(ranger_id),
+    sighting_time TIMESTAMP,
+    location VARCHAR(100),
+    notes TEXT
+   );
+
+ 
+
 
  INSERT INTO rangers (name, region) VALUES 
 ('Alice Green', 'Northern Hills'),
@@ -33,7 +45,20 @@ INSERT INTO species (common_name, scientific_name, discovery_date, conservation_
 ('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
 ('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
 
-SELECT * FROM rangers ;
+
+-- Insert sightings
+INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VALUES
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL),
+(2, 1, 'Riverbend Trail', '2024-05-20 08:15:00', 'Tracks found near river'),
+(3, 2, 'Eastern Bamboo Trail', '2024-05-21 13:00:00', 'Adult seen resting'),
+(4, 3, 'Forest Edge', '2024-05-22 19:45:00', 'Heard trumpet call');
 
 
-SELECT * FROM species ;
+SELECT * FROM sightings;
+
+
+
+
