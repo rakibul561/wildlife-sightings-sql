74e89a94be5574bca2d240036efa2271164d6584
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-05-24 23:14:26 -0700

    some change code

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,14 +1,12 @@
 -- Active: 1747583459747@@127.0.0.1@5432@conservation_db
 
-
-
-CREATE DATABASE  conservation_db ;
-
- CREATE Table rangers (
-    ranger_id SERIAL  PRIMARY KEY ,
+CREATE DATABASE conservation_db;
+-- Create rangers table
+CREATE Table rangers (
+    ranger_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     region VARCHAR(100)
- );
+);
 
 -- Create species table
 CREATE TABLE species (
@@ -19,19 +17,18 @@ CREATE TABLE species (
     conservation_status VARCHAR(50)
 );
 
-   -- Create  sightings table
-   CREATE TABLE sightings (
+DROP TABLE sightings;
+-- Create  sightings table
+CREATE TABLE sightings (
     sighting_id SERIAL PRIMARY KEY,
-    species_id INTEGER REFERENCES species(species_id),
-    ranger_id  INTEGER REFERENCES rangers(ranger_id),
+    species_id INTEGER REFERENCES species (species_id),
+    ranger_id INTEGER REFERENCES rangers (ranger_id),
     sighting_time TIMESTAMP,
     location VARCHAR(100),
     notes TEXT
-   );
+);
 
- 
-
-
+-- Insert rangers
  INSERT INTO rangers (name, region) VALUES 
 ('Alice Green', 'Northern Hills'),
 ('Bob White', 'River Delta'),
@@ -51,14 +48,49 @@ INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VA
 (1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
 (2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
 (3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
-(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL),
-(2, 1, 'Riverbend Trail', '2024-05-20 08:15:00', 'Tracks found near river'),
-(3, 2, 'Eastern Bamboo Trail', '2024-05-21 13:00:00', 'Adult seen resting'),
-(4, 3, 'Forest Edge', '2024-05-22 19:45:00', 'Heard trumpet call');
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
+
+----- problem 1 -------------
+INSERT INTO
+    rangers (name, region)
+VALUES ('Derek Fox', 'Coastal Plains')
+
+--------problem 2 ----------
+SELECT COUNT(DISTINCT species_id) AS unique_species_count
+FROM sightings;
+
+-------Problem 3 -------------
+SELECT
+    sighting_id,
+    species_id,
+    ranger_id,
+    sighting_time,
+    location,
+    notes
+FROM sightings
+WHERE
+    location LIKE '%Pass%';
+
+-----problem 4 ----------
+
+SELECT rangers.name, COUNT(sightings.ranger_id) AS total_sightings
+FROM rangers
+    INNER JOIN sightings ON rangers.ranger_id = sightings.ranger_id
+GROUP BY rangers.name;
 
 
-SELECT * FROM sightings;
+--------problem 5 ----------
+
+ SELECT * FROM species 
+ WHERE species_id NOT IN (
+    SELECT species_id FROM sightings 
+ )
+ ADD
+ -----problem 7 ---------
+
+  UPDATE species
+  SET conservation_status ='Historic'
+  WHERE EXTRACT(year FROM  discovery_date) < '1800';
 
 
-
-
+---------problem 8 -----------
